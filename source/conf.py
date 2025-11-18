fe6898e4d1f3e405a1c21879be7dd66c16ac0417
Tobias Buck <buck@mpia.de>
2025-11-18 21:58:17 +0100

    Update copyright and author information in conf.py

--- source/conf.py
+++ source/conf.py
@@ -12,8 +12,8 @@ sys.path.insert(0, os.path.abspath("../"))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "rubix"
-copyright = "2024, Ufuk, Tobias, Anna Lena"
-author = "Ufuk, Tobias, Anna Lena"
+copyright = "2025, AstroAI Lab"
+author = "Anna Lena Schaible, Ufuk Çakır, Tobias Buck"
 release = "0.1"
 
 # -- General configuration ---------------------------------------------------
